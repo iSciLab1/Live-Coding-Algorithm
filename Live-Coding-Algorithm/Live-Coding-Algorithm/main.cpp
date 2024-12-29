60d5ec9a16ca40ea4830667a255f90afd445b5aa
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-12-30 00:31:50 +0100

    Feat: added send signal, finbonacci algorithm and reverse string

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -1,70 +1,51 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <vector>
-#include <cmath>
-
-#define PI 3.1415
-
 using namespace std;
 
-double calculateBER(const std::vector<int>& sent, const std::vector<int>& received);
-
-class AWGSimulator {
-private:
-    double _frequency;
-    double _amplitude;
-    int _samples;
-    std::vector<double> signalBuffer;
-
-public:
-    AWGSimulator(double frequency, double amplitude, int samples) :
-        _frequency(frequency), _amplitude(amplitude), _samples(samples), signalBuffer(_samples, 0) {}
-
-    std::vector<double> getSignal() {
-        return signalBuffer;
-    }
-
-    void generateSineWave() {
-        signalBuffer.clear();
-        signalBuffer.reserve(_samples);
-        for (int i = 0; i < _samples; i++) {
-            signalBuffer.push_back(_amplitude * sin(2 * PI * _frequency * i / _samples));
+void sendSignal(const std::vector<int>& signal) {
+    // Sendet ein Signal an das Gerät
+    for (int bit : signal) {
+        std::cout << "Sending bit: " << bit << std::endl;
+        int input = 0;
+        if (bit != 0 && bit != 1) {
+            std::cerr << "Error: Invalid bit detected!" << std::endl;
+            std::cerr << "Corrector: Enter the correct bit!" << std::endl;
+            std::cin >> input;
+            bit = input;
+            std::cout << "Corrected bit: " << input << std::endl;
         }
     }
-};
-
-int main() {
-    //AWGSimulator awg(1.0, 5.0, 10);
-    //awg.generateSineWave();
-    //std::vector<double> signal = awg.getSignal();
-
-    //for (size_t i = 0; i < signal.size(); i++) {
-    //    std::cout << "Sinus Wave " << i << ": " << signal[i] << std::endl;
-    //}
-
-
-     std::vector<int> sent = {1, 1, 1, 1};
-     std::vector<int> received = {0, 0, 0, 0};
-     try {
-         double ber = calculateBER(sent, received);
-         std::cout << "BER: " << ber << std::endl;
-     } catch (const std::exception& e) {
-         std::cerr << e.what() << std::endl;
-     }
-
-    return 0;
+    std::cout << "Signal sent successfully." << std::endl;
 }
 
-double calculateBER(const std::vector<int>& sent, const std::vector<int>& received) {
-    if (sent.size() != received.size()) {
-        throw std::invalid_argument("Vectors must be of the same size");
+int fibonacci(int num) {
+    if (num <= 1) {
+        return num;
+    }
+    else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 
-    int numErr = 0;
-    int numBit = sent.size();
-    for (int i = 0; i < numBit; i++) {
-        if (sent[i] != received[i]) {
-            numErr++;
-        }
+}
+
+string reverseString(string s) {
+    string rs;
+    for (int i = s.size() - 1; i >= 0; i--) {
+
+        rs += s[i];
     }
-    return double(numErr) / double(numBit);
+    return rs;
+
+}
+
+
+int main() {
+    //std::vector<int> signal = { 1, 0, 1, 2, 0, 1 }; 
+    //sendSignal(signal);
+
+    //std::cout << fibonacci(7) << std::endl;
+    std::cout << reverseString("hello") << std::endl;
+
+    return 0;
 }
