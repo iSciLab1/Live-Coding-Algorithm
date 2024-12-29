39c36f7dd30b8121508cb8ecf1d06e19ce3dbc01
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-12-30 00:46:24 +0100

    Feat: added rectangle perimeter and area calculator

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -3,49 +3,28 @@
 #include <vector>
 using namespace std;
 
-void sendSignal(const std::vector<int>& signal) {
-    // Sendet ein Signal an das Gerät
-    for (int bit : signal) {
-        std::cout << "Sending bit: " << bit << std::endl;
-        int input = 0;
-        if (bit != 0 && bit != 1) {
-            std::cerr << "Error: Invalid bit detected!" << std::endl;
-            std::cerr << "Corrector: Enter the correct bit!" << std::endl;
-            std::cin >> input;
-            bit = input;
-            std::cout << "Corrected bit: " << input << std::endl;
-        }
+class Reactangle {
+private:
+    int width;
+    int height;
+public:
+    Reactangle(int _width, int  _height) : width(_width), height(_height) {}
+    int area() {
+        return width * height;
     }
-    std::cout << "Signal sent successfully." << std::endl;
-}
-
-int fibonacci(int num) {
-    if (num <= 1) {
-        return num;
-    }
-    else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+    int perimeter() {
+        return 2 * (width + height);
     }
 
-}
-
-string reverseString(string s) {
-    string rs;
-    for (int i = s.size() - 1; i >= 0; i--) {
-
-        rs += s[i];
-    }
-    return rs;
-
-}
-
-
+};
 int main() {
-    //std::vector<int> signal = { 1, 0, 1, 2, 0, 1 }; 
-    //sendSignal(signal);
 
-    //std::cout << fibonacci(7) << std::endl;
-    std::cout << reverseString("hello") << std::endl;
+    Reactangle reactangle( 2, 2);
+    int resultArea = reactangle.area();
+    int resultPerimeter = reactangle.perimeter();
+
+    std::cout << "Fläche: " << resultArea << std::endl;
+    std::cout << "Umfang: " << resultPerimeter << std::endl;
 
     return 0;
 }
