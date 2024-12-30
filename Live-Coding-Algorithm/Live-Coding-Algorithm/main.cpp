378ccaa6687d5426d0c607e89bba141d7a892c5a
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-12-30 19:39:48 +0100

    Fetat: added inheritance example

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -1,16 +1,41 @@
 ﻿
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <math.h>
+#define M_PI 3.141592
+
 using namespace std;
 
-class Reactangle {
+class Shape {
+
+private:
+public:
+    virtual double getArea() {
+        return 0;
+    }
+    virtual ~Shape() {};
+};
+
+class Circle: public Shape {
+
+private:
+    double radius;
+public:
+    Circle(double _radius): radius(_radius){}
+    double getArea() {
+        return M_PI * radius * radius;
+    }
+};
+
+class Reactangle: public Shape {
 private:
     int width;
     int height;
 public:
     Reactangle(int _width, int  _height) : width(_width), height(_height) {}
     Reactangle(): width(1), height(1){}
-    int area() {
+    double getArea() {
         return width * height;
     }
     int perimeter() {
@@ -19,21 +44,13 @@ public:
 
 };
 int main() {
-    Reactangle reactangle_def;
-    int resultArea = reactangle_def.area();
-    int resultPerimeter = reactangle_def.perimeter();
+    Shape* shape = new Reactangle(2,2);
+    Shape* shape_def = new Reactangle();
+    Shape* shape_cir = new Circle(2);
 
-    std::cout << "Fläche: " << resultArea << std::endl;
-    std::cout << "Umfang: " << resultPerimeter << std::endl;
-
-
-    Reactangle reactangle( 2, 2);
-    resultArea = reactangle.area();
-    resultPerimeter = reactangle.perimeter();
-
-    std::cout << "Fläche: " << resultArea << std::endl;
-    std::cout << "Umfang: " << resultPerimeter << std::endl;
-
+    std::cout << "Fläche: " << shape->getArea() << std::endl;
+    std::cout << "Fläche: " << shape_def->getArea() << std::endl;
+    std::cout << "Fläche: " << shape_cir->getArea() << std::endl;
 
     return 0;
 }
