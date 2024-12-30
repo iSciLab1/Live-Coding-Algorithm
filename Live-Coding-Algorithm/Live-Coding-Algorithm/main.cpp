f2824cf8ac3ede4175e0a82d1779d21c90034acc
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-12-30 21:20:42 +0100

    Feat: added abstract class and destructor

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -1,40 +1,27 @@
-﻿
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <math.h>
-#include <complex>
-
-
-#define M_PI 3.141592
-
+﻿#include <iostream>
 using namespace std;
-//using namespace complex_literals;
 
-class Complex {
-private:
-    double real;
-    double image;
+class Shape {
 public:
-    Complex(double _real, double _image): real(_real), image(_image){}
-    Complex operator+(const Complex& other) const {
-        return { real + other.real, image + other.image };
+    virtual double getArea() = 0;
+    virtual ~Shape() {
+        cout << "Shape destroyed" << endl;
     }
-    //void print() const {
-    //    cout << real << " + " << image << "i";
-    //}
-    friend ostream& operator<<(ostream& out, const Complex& c) {
-        out << c.real << " + " << c.image << "i";
-        return out;
-    }
+};
 
+class Rectangle : public Shape {
+public:
+    ~Rectangle() {
+        cout << "Rectangle destroyed" << endl;
+    }
+    double getArea() override {
+        return 0; 
+    }
 };
 
 int main() {
-    Complex c1(2, 2);
-    Complex c2(1, 1);
-    Complex c3 = c1 + c2;
-    cout << "Summe: " << c3 << endl;
-    //c3.print();
+    Shape* rect = new Rectangle();
+    delete rect;
+
     return 0;
 }
