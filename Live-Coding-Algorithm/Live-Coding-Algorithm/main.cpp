eb42663896f79c4faafe709b3c7760b18bfa01ad
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-12-30 20:21:56 +0100

    Feat: added class complex

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -3,81 +3,29 @@
 #include <vector>
 #include <cmath>
 #include <math.h>
+#include <complex>
+
+
 #define M_PI 3.141592
 
 using namespace std;
+//using namespace complex_literals;
 
-class Shape {
-
+class Complex {
 private:
+    std::complex<double> z1;
+    std::complex<double> z2;
 public:
-    virtual double getArea() {
-        return 0;
-    }
-    virtual void printArea() {
-        std::cout << "Fläche: " << getArea() << std::endl;
-    }
-    virtual ~Shape() {};
-};
-
-class Circle: public Shape {
-
-private:
-    double radius;
-    double result;
-public:
-    Circle(double _radius): radius(_radius){}
-    double getArea() override {
-        result = M_PI * radius * radius;
-        return result;
-    }
-    void printArea() override {
-        std::cout << "Fläche: " << getArea() << std::endl;
+    Complex(complex<double> _z1, complex<double> _z2): z1(_z1), z2(_z2){}
+    std::complex<double> addition(){
+        return z1 + z2;
     }
 };
 
-class Reactangle: public Shape {
-private:
-    int width;
-    int height;
-    double result;
-public:
-    Reactangle(int _width, int  _height) : width(_width), height(_height) {}
-    Reactangle(): width(1), height(1){}
-    double getArea() override {
-        result = width * height;
-        return result;
-    }
-    int perimeter() {
-        return 2 * (width + height);
-    }
-    void printArea() override {
-        std::cout << "Fläche: " << getArea() << std::endl;
-    }
-
-};
 int main() {
-    std::vector<Shape*> shapes{new Reactangle(), new Reactangle(2,2), new Circle(2) };
-    for (int i = 0; i < shapes.size(); i++) {
-        shapes[i]->printArea();
-    }
-    for (int i = 0; i < shapes.size(); i++) {
-        delete shapes[i];
-    }
+    Complex complex(1.0 + 2.0i, 2.0 + 1.0i);
 
-    vector<Shape*> shapes_v2;
-    shapes_v2.push_back(new Reactangle());
-    shapes_v2.push_back(new Reactangle(2,2));
-    shapes_v2.push_back(new Circle(2));
-
-    for (Shape* shape : shapes_v2) {
-        shape->printArea();
-    }
-
-    for (Shape* shape : shapes_v2) {
-        delete shape;
-    }
-
-
+    cout << "Complex: " << complex.addition() << endl;
+    
     return 0;
 }
