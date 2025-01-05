ebed98689a185fc529922e37f56bb096dbc160e9
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2025-01-05 20:53:04 +0100

    Feat: added binary tree

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -18,14 +18,35 @@ void quickSort(vector<int>& array, int low, int high) {
     }
 }
 
+int binarySearch(const vector<int>& array, int target, int left, int right) {
+    if (left > right) {
+        return -1; 
+    }
+
+    int mid = left + (right - left) / 2; 
+
+    if (target == array[mid] ) {
+        return mid; 
+    }
+    else if ( target < array[mid]) {
+        return binarySearch(array, target, left, mid - 1); 
+    }
+    else {
+        return binarySearch(array, target, mid + 1, right); 
+    }
+}
 
 int main() {
     vector<int> array = { 1, 2, 5, 3, 5, 3, 4, 7, 9, 0, 3 };
-
     quickSort(array, 0, array.size() - 1);
-
     for (int num : array) {
         cout << num << " ";
     }
+
+
+    cout << endl;
+    int target = 7;
+    int result = binarySearch(array, target, 0, array.size() - 1);
+    cout << "Element: " << result << endl;
     return 0;
 }
