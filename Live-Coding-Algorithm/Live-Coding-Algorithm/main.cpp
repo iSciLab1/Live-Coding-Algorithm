bb292f74d4d35a7f2b0ae04be7a14352e753bfb8
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2025-01-05 20:01:11 +0100

    Feat: added quick sort

--- Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
+++ Live-Coding-Algorithm/Live-Coding-Algorithm/main.cpp
@@ -1,41 +1,31 @@
-﻿#include <vector>
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 using namespace std;
 
-int majorityElement(vector<int> nums) {
-    int candidate = -1, count = 0;
-
-    // Phase 1: Find the candidate
-    for (int num : nums) {
-        if (count == 0) {
-            candidate = num;
-            count = 1;
+void quickSort(vector<int>& array, int low, int high) {
+    if (low < high) {
+        int pivot = array[high];
+        int idx = low;
+        for (int i = low; i < high; i++) {
+            if (array[i] < pivot) {
+                swap(array[idx], array[i]);
+                idx++;
+            }
         }
-        else if (num == candidate) {
-            count++;
-        }
-        else {
-            count--;
-        }
+        swap(array[idx], array[high]);
+        quickSort(array, low, idx -1 );
+        quickSort(array, idx + 1, high);
     }
-
-    // Phase 2: Verify the candidate
-    count = 0;
-    for (int num : nums) {
-        if (num == candidate){
-            count++;
-        }
-    }
-
-    if (count > nums.size() / 2) {
-        return candidate;
-    }
-    return -1;
 }
 
-int main() {
-    vector<int> nums = { 2, 1, 3, 3, 1, 1, 3, 2, 1};
-    cout << "Majority Element: " << majorityElement(nums) << endl;
 
+int main() {
+    vector<int> array = { 1, 2, 5, 3, 5, 3, 4, 7, 9, 0, 3 };
+
+    quickSort(array, 0, array.size() - 1);
+
+    for (int num : array) {
+        cout << num << " ";
+    }
     return 0;
 }
